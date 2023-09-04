75080c88b915c9cad7674b9670acfe6090fa28b1
Xindref <dakotahmccrary@gmail.com>
2023-09-03 23:34:55 -0500

    project started, home page complete

---
+++ src/icons/index.js
@@ -0,0 +1,6 @@
+import astronaut from "./astronaut.png";
+import planetEarth from "./planet-earth.png";
+import rocket from "./rocket.png";
+import wrench from "./wrench.png";
+
+export { astronaut, planetEarth, rocket, wrench };
